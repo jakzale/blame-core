9ac2f672a53d8eec06e77a3ad8955beeab9e5cfa
Jakub Z <jakzale@gmail.com>
2014-10-14 16:42:54 +0100

    Add working gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@
 var gulp  = require('gulp'),
     shell = require('gulp-shell'),
     del   = require('del'),
-    files;
+    karma = require('karma').server,
+    paths;
 
-files = {
-  src: 'src',
+paths = {
+  src: 'src/*.ts',
   build: 'build'
 };
 
@@ -17,17 +18,30 @@ gulp.task('clean', function(cb) {
 });
 
 gulp.task('build', ['clean'], function() {
-  return gulp.src(files.src + '/*.ts', {read: false})
+  return gulp.src(paths.src, {read: false})
     .pipe(shell([
       './node_modules/typescript/bin/tsc -t ES5 -m commonjs <%= file.path %> --outDir <%= out %>'
     ], {
       templateData: {
-        out: files.build
+        out: paths.build
       }
     }));
 });
 
-gulp.task('default', ['clean', 'build']);
+gulp.task('watch', function() {
+  gulp.watch(paths.src, ['build']);
+});
+
+
+// Warning, karma is a rolling task
+// Secondly, there is a potential of many cache invalidations
+gulp.task('karma', ['build'], function() {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+  });
+});
+
+gulp.task('default', ['watch', 'karma']);
 
 
 
