3d4a3b75f033ca73d80bd0d47ad2e7444af24401
Jakub Z <jakzale@gmail.com>
2014-10-14 16:21:44 +0100

    Add running typescript to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,32 @@
 'use strict';
 
 
-var gulp = require('gulp');
+var gulp  = require('gulp'),
+    shell = require('gulp-shell'),
+    del   = require('del'),
+    files;
 
+files = {
+  src: 'src',
+  build: 'build'
+};
 
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
+});
 
+gulp.task('build', ['clean'], function() {
+  return gulp.src(files.src + '/*.ts', {read: false})
+    .pipe(shell([
+      './node_modules/typescript/bin/tsc -t ES5 -m commonjs <%= file.path %> --outDir <%= out %>'
+    ], {
+      templateData: {
+        out: files.build
+      }
+    }));
+});
+
+gulp.task('default', ['clean', 'build']);
 
 
 
